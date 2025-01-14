9c43f6318fa9ce175b7a2e60e7c89ff1fc0e0afb
Supun <126235564+SupunCharindu@users.noreply.github.com>
2025-01-14 08:53:45 +0100

    Ceasor cipher project includes comments

---
+++ Project_01/cipher_ass.rb
@@ -0,0 +1,28 @@
+=begin
+Implement a Caesar cipher that takes in a string and the shift factor and then outputs
+the modified string using a right shift.
+=end
+
+def cipher(string,shift)
+  modified_str = ''
+
+  string.each_char do |char|
+    ascii = char.ord #make string to ord value(ASCII)
+
+    if(ascii.between?(65,90) || ascii.between?(97,122))
+      new_ascii = ascii + shift
+
+      if (new_ascii > 90 && ascii.between?(65,90) || new_ascii > 122 && ascii.between?(97,122))
+        new_ascii -=26
+      end
+
+      cipher_char = new_ascii.chr
+      modified_str << cipher_char
+    else
+      modified_str << char #characters that not a letters
+    end
+  end
+  puts modified_str
+end
+
+cipher("What a string!",5)
